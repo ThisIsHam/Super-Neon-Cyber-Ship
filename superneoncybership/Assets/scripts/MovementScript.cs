8452b88f0cc6660e37f6e5da8f4edb7b0cd2058f
ThisIsHam <ThisIsHam>
2014-07-14 13:04:43 -0400

    First Build
    First working build- movement/wall collisions

---
+++ superneoncybership/Assets/scripts/MovementScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigidbody.velocity=new Vector3(Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")) * speed;
+
+	}
+}
